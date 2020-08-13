e7a54ae3c8067b736f2bb40e300560581055ee70
EzehMilton <mccezeh@gmail.com>
2020-08-13 08:30:14 +0100

    Added recordbp service

---
+++ recordbp/src/main/java/com/milton/recordbp/RecordbpApplication.java
@@ -0,0 +1,13 @@
+package com.milton.recordbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordbpApplication.class, args);
+	}
+
+}
