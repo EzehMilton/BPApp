05c19338ee224598eee8b2a4107e5f5e1f69adf7
EzehMilton <mccezeh@gmail.com>
2020-08-19 16:24:52 +0100

    Cleaned up code.

--- recordbp/src/main/java/com/milton/recordbp/controller/RecordBpController.java
+++ recordbp/src/main/java/com/milton/recordbp/controller/RecordBpController.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
